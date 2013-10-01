0367980e0614a48c0739cae9d64f4875f2d23bb1
Daniel Dimitrov <daniel@compojoom.com>
2013-10-01 14:03:23 +0200

    phpcs

--- source/plugins/system/mandrill/mandrill.php
+++ source/plugins/system/mandrill/mandrill.php
@@ -1,16 +1,27 @@
 <?php
 /**
- * @author Daniel Dimitrov
+ * @author     Daniel Dimitrov <daniel@compojoom.com>
+ * @date       22.02.13
  *
- * @copyright  Copyright (C) 2008 - 2012 compojoom.com, Inc. All rights reserved.
+ * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
-// no direct access
+
+// No direct access
 defined('_JEXEC') or die('Restricted access');
 
-
-class plgSystemMandrill extends JPlugin
+/**
+ * Class plgSystemMandrill
+ *
+ * @since  1.0
+ */
+class PlgSystemMandrill extends JPlugin
 {
+	/**
+	 * Here we will override the JMail class
+	 *
+	 * @return bool
+	 */
 	public function onAfterInitialise()
 	{
 		$params = JComponentHelper::getParams('com_cmandrill');
@@ -20,18 +31,21 @@ class plgSystemMandrill extends JPlugin
 
 		$key = $params->get('apiKey');
 
-		if (strlen($key)) {
-
+		if (strlen($key))
+		{
 			$path = JPATH_ROOT . '/plugins/system/mandrill/mailer/mail.php';
 
 			JLoader::register('JMail', $path);
 			JLoader::load('JMail');
-
-		} else {
+		}
+		else
+		{
 			$appl->enqueueMessage(JText::sprintf('PLG_SYSTEM_MANDRILL_NO_API_KEY_SPECIFIED', JRoute::_('index.php?option=com_cmandrill')), 'warning');
+
 			return false;
 		}
-	}
 
+		return true;
+	}
 }
 
