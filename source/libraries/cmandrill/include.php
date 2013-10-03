8400c623f2de4049d37de785b6daf4fec609f31f
Daniel Dimitrov <daniel@compojoom.com>
2013-10-03 17:01:30 +0200

    adding a mandrill library

---
+++ source/libraries/cmandrill/include.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package    com_cmandrill
+ * @author     DanielDimitrov <daniel@compojoom.com>
+ * @date       03.10.13
+ *
+ * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+if (!defined('CMANDRILL_INCLUDED'))
+{
+	define('CMANDRILL_INCLUDED', '3.0.0');
+
+// Register the FOF autoloader
+	require_once __DIR__ . '/autoloader/autoloader.php';
+	CMandrillAutoloader::init();
+}
