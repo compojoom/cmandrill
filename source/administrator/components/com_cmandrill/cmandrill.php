1b1e91b29a267fb439da08affcd861b9b518d70e
Daniel Dimitrov <daniel@compojoom.com>
2013-11-06 15:46:37 +0100

    phpcs

--- source/administrator/components/com_cmandrill/cmandrill.php
+++ source/administrator/components/com_cmandrill/cmandrill.php
@@ -1,38 +1,40 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 14.01.13
+ * @author     Daniel Dimitrov <daniel@compojoom.com>
+ * @date       14.01.13
  *
  * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
- 
+
 defined('_JEXEC') or die('Restricted access');
 
 // Access check.
-if (!JFactory::getUser()->authorise('core.manage', 'com_cmandrill')) {
+if (!JFactory::getUser()->authorise('core.manage', 'com_cmandrill'))
+{
 	throw new Exception(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
-// first lets make sure that all the lang files are loaded
-// thank you for this black magic Nickolas :)
 // Magic: merge the default translation with the current translation
 $jlang = JFactory::getLanguage();
 $jlang->load('com_cmandrill', JPATH_ADMINISTRATOR, 'en-GB', true);
 $jlang->load('com_cmandrill', JPATH_ADMINISTRATOR, $jlang->getDefault(), true);
 $jlang->load('com_cmandrill', JPATH_ADMINISTRATOR, null, true);
 
-JLoader::discover('cmandrillHelper', JPATH_COMPONENT_ADMINISTRATOR.'/helpers/');
+JLoader::discover('cmandrillHelper', JPATH_COMPONENT_ADMINISTRATOR . '/helpers/');
 JTable::addIncludePath(JPATH_COMPONENT . '/tables');
 
 $input = JFactory::getApplication()->input;
-if($input->getCmd('view','') == 'liveupdate') {
-	JToolBarHelper::preferences( 'com_cmandrill' );
+
+if ($input->getCmd('view', '') == 'liveupdate')
+{
+	JToolBarHelper::preferences('com_cmandrill');
 	LiveUpdate::handleRequest();
+
 	return;
 }
 
-// on 2.5 we need the bootstrap css so let's add it here
+// On 2.5 we need the bootstrap css so let's add it here
 cmandrillHelperUtility::bootstrap();
 
 $controller = JControllerLegacy::getInstance('Cmandrill');
