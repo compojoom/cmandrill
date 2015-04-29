ef4eb07f5cd15f86e18aee15365db27c1648ceea
Daniel Dimitrov <daniel@compojoom.com>
2015-04-29 21:11:06 +0200

    updating config and wrong views

--- source/administrator/components/com_cmandrill/views/dashboard/tmpl/default_config.php
+++ source/administrator/components/com_cmandrill/views/dashboard/tmpl/default_config.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 14.01.13
+ * @author     Daniel Dimitrov - compojoom.com
+ * @date       : 14.01.13
  *
  * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
@@ -10,25 +10,28 @@
 defined('_JEXEC') or die('Restricted access');
 
 JHtml::stylesheet('media/com_cmandrill/css/dashboard.css');
+echo CompojoomHtmlCtemplate::getHead(CMandrillHelperMenu::getMenu(), 'dashboard', 'COM_CMANDRILL_DASHBOARD', '');
 ?>
-<div class="compojoom-bootstrap">
-	<div id="j-main-container" class="span12 row-fluid">
-		<div class="span12">
-			<div class="span2" style="height: 256px; padding-top: 70px;">
-				<h1 style="font-weight: bold; color:#ff0000; text-align: center">Don't panic</h1>
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="box-info">
+				<h2>Don't panic!</h2>
 				<?php echo JText::sprintf('COM_CMANDRILL_GET_API_KEY', 'http://mandrillapp.com'); ?>
-				<br/>
+				<br />
 				<?php echo JText::sprintf('COM_CMANDRILL_READ_THE_DOCS', 'https://compojoom.com/support/documentation/mandrill'); ?>
-			</div>
-			<div class="android offset2">
 
-			</div>
+				<div class="android offset2">
 
-			<?php echo cmandrillHelperUtility::footer(); ?>
-			<div class="small">
-				Robot image was taken from <a href="http://openclipart.org/detail/20510/-by--20510" target="_blank">openclipart.org</a>.
+				</div>
+
+				<?php echo cmandrillHelperUtility::footer(); ?>
+				<div class="small">
+					Robot image was taken from
+					<a href="http://openclipart.org/detail/20510/-by--20510" target="_blank">openclipart.org</a>.
+				</div>
 			</div>
 		</div>
-
 	</div>
-</div>
+<?php
+// Show Footer
+echo CompojoomHtmlCTemplate::getFooter(CmandrillHelperUtility::footer());
